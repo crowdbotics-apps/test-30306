0b450c1ac96bc168894a19bb755a829e8e7b06e2
Crowdbotics <team@crowdbotics.com>
2021-09-04 00:10:20 +0000

    Update models

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from .models import User
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 
 from users.forms import UserChangeForm, UserCreationForm
 
 User = get_user_model()
+admin.site.register(User)
