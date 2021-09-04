33ff0ba31d8ab80954396a044c804347e32dda3e
Crowdbotics <team@crowdbotics.com>
2021-09-04 00:06:24 +0000

    Update models

--- users/models.py
+++ users/models.py
@@ -23,6 +23,3 @@ class User(AbstractUser):
         blank=True,
         max_length=255,
     )
-
-    def get_absolute_url(self):
-        return reverse("users:detail", kwargs={"username": self.username})
